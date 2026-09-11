f66c5270a618609c7de9bfab4099ef758b563367
Devesh <kadamdevesh969@gmail.com>
2026-09-11 12:02:36 +0530

    CORS changes done

--- backend/app/main.py
+++ backend/app/main.py
@@ -25,10 +25,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://insight-flow-lemon-phi.vercel.app"
-    ],
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://insight-flow-lemon-phi.vercel.app",
+    "https://insight-flow-final.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
